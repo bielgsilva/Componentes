ae7810b8cd2dd21017c781e8082eb4da3da4ffdb
Gabriel Gomes Silva <ggsilva.eng@gmail.com>
2023-10-05 22:37:35 -0300

    feat: rename

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,8 +1,10 @@
 import './styles.css'
+import CreditCardForm from './components/credit card';
 const Main = () => {
   return (
     <div className="main overlay">
       <div className='main-content overlay1 flex-center'>
+        <CreditCardForm />
       </div>
     </div>
   );
